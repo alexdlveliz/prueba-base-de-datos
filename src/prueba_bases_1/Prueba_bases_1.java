a4b3d57d30e45f4ea1d5ece4e0070af189a34c17
alexd <alexd@192.168.56.1>
2018-01-23 22:27:19 -0600

    Yo hice el hola alex en Prueba_bases_1.java

--- src/prueba_bases_1/Prueba_bases_1.java
+++ src/prueba_bases_1/Prueba_bases_1.java
@@ -17,6 +17,7 @@ public class Prueba_bases_1 {
     public static void main(String[] args) 
     {
         System.out.println("Hola mundo");
+        System.out.println("Hola alex");
     }
     
 }
